1929f6b8d383e0d1fa68e405707028f8be034eec
Yanova <riefkyanova@gmail.com>
2024-04-01 13:46:39 +0700

    completion

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
-'use strict'
+"use strict";
 
-const showModal = document.querySelector('.show-modal');
-const btnClose = document.querySelector('.close');
-const modal = document,querySelector('.modal');
+const btnModals = document.querySelectorAll(".show-modal");
+const btnClose = document.querySelector(".close");
+const modal = document.querySelector(".modal");
+
+const showModal = () => {
+  modal.classList.remove("hidden");
+};
+
+const hideModal = () => {
+  modal.classList.add("hidden");
+};
+
+for (let i = 0; i < btnModals.length; i++) {
+  btnModals[i].addEventListener("click", showModal);
+}
+
+btnClose.addEventListener("click", hideModal);
+
+modal.addEventListener("click", hideModal);
+
+document.addEventListener("keydown", function (e) {
+  if (e === "Escape" && !modal.classList.contains("hidden")) {
+    hideModal();
+  }
+});
